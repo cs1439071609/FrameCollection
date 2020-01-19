edf978cfac7de0cba47938b021cfad8ae6756f4a
chensen <1439071609@qq.com>
2020-01-19 16:26:35 +0800

    初始化项目

---
+++ app/src/main/java/pers/cs/framecollection/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package pers.cs.framecollection.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pers.cs.framecollection.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/pers/cs/framecollection/ui/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package pers.cs.framecollection.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import pers.cs.framecollection.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
